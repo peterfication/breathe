e220993e0699cd1ce80a2f0d9cf42333f6ba62eb
Peter Gundel <mail@petergundel.de>
2022-05-14 15:06:12 +0100

    Extract breath.Runner struct

--- breathe/cycle.go
+++ breathe/cycle.go
@@ -20,32 +20,60 @@ type BreathCycle struct {
 
 // Take one BreathCycle and create a slice of the same BreathCycles with
 // the cyclesCount of it.
-func GenerateBreathCycles(cycle BreathCycle, cyclesCount int) (cycles []BreathCycle) {
+func GenerateBreathCycles(cycle BreathCycle, cyclesCount int) (breathCycles []BreathCycle) {
 	for i := 0; i < cyclesCount; i++ {
-		cycles = append(cycles, cycle)
+		breathCycles = append(breathCycles, cycle)
 	}
-	return cycles
+	return breathCycles
 }
 
-// Draw the UI and kick off the breath cycles
-func RunBreathCycles(title string, cycles []BreathCycle, sound string) {
+type Runner struct {
+	title string
+	sound string
+
+	breathCycles []BreathCycle
+
+	// Widgets
+	gaugeChart *widgets.Gauge
+	textBox    *widgets.Paragraph
+
+	// Closures
+	renderText func(currentCycleCount int)
+	playSound  func(soundName string)
+}
+
+func (runner *Runner) Init() {
 	if err := ui.Init(); err != nil {
 		log.Fatalf("Failed to initialize TermUI: %v", err)
 	}
-	defer ui.Close()
 
-	gaugeChart := initGaugeChart()
-	textBox := initTextBox()
-	renderText := createRenderText(textBox, title, cycles)
-	playSound := initSpeaker(sound)
+	runner.gaugeChart = initGaugeChart()
+	runner.textBox = initTextBox()
+	runner.renderText = createRenderText(runner.textBox, runner.title, runner.breathCycles)
+	runner.playSound = initSpeaker(runner.sound)
+}
 
-	go runBreathCycles(cycles, gaugeChart, renderText, playSound)
+func (runner *Runner) Run() {
+	go runBreathCycles(runner.breathCycles, runner.gaugeChart, runner.renderText, runner.playSound)
 
 	for e := range ui.PollEvents() {
 		if e.Type == ui.KeyboardEvent {
 			break
 		}
 	}
+}
+
+func (runner *Runner) Close() {
+	ui.Close()
+}
+
+// Init the Runner, run it and close it afterwards
+func RunBreathCycles(title string, breathCycles []BreathCycle, sound string) {
+	runner := Runner{title: title, breathCycles: breathCycles, sound: sound}
+	runner.Init()
+	defer runner.Close()
+
+	runner.Run()
 }
 
 // The gauge chart that displays the breathing action to perform
@@ -74,23 +102,23 @@ func initTextBox() *widgets.Paragraph {
 }
 
 // The closure that refreshes the text box by re-rendering it
-func createRenderText(textBox *widgets.Paragraph, title string, cycles []BreathCycle) func(currentCycleCount int) {
+func createRenderText(textBox *widgets.Paragraph, title string, breathCycles []BreathCycle) func(currentCycleCount int) {
 	return func(currentCycleCount int) {
 		text := fmt.Sprintf(`Always inhale through the nose!
 
 %s
 Total duration: %s
 Cycle %d of %d
-`, title, totalDuration(cycles), currentCycleCount, len(cycles))
+`, title, totalDuration(breathCycles), currentCycleCount, len(breathCycles))
 		textBox.Text = text
 		ui.Render(textBox)
 	}
 }
 
 // Sum up the duration of all steps in all cycles
-func totalDuration(cycles []BreathCycle) time.Duration {
+func totalDuration(breathCycles []BreathCycle) time.Duration {
 	totalDurationMilliseconds := int64(0)
-	for _, cycle := range cycles {
+	for _, cycle := range breathCycles {
 		totalDurationMilliseconds += cycle.Inhale.Milliseconds() +
 			cycle.InhaleHold.Milliseconds() +
 			cycle.Exhale.Milliseconds() +
@@ -101,8 +129,8 @@ func totalDuration(cycles []BreathCycle) time.Duration {
 }
 
 // Run the breath cycles
-func runBreathCycles(cycles []BreathCycle, gaugeChart *widgets.Gauge, renderText func(currentCycleCount int), playSound func(soundName string)) {
-	for i, cycle := range cycles {
+func runBreathCycles(breathCycles []BreathCycle, gaugeChart *widgets.Gauge, renderText func(currentCycleCount int), playSound func(soundName string)) {
+	for i, cycle := range breathCycles {
 		renderText(i + 1)
 		runBreatheSubCycle("Inhale", cycle.Inhale, gaugeChart, playSound)
 		if cycle.InhaleHold.Milliseconds() > 0 {
